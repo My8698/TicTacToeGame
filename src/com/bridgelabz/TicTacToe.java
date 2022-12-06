14eca8e05fe885abba809901d32f36f52705df73
devraajmadhuri@gmail.com <devraajmadhuri@gmail.com>
2022-12-06 13:34:17 +0530

    Added:ability to choose letter X or O to player

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -1,9 +1,15 @@
 package com.bridgelabz;
+
+import java.util.Scanner;
+
 public class TicTacToe {
      /**
          * create a gameboard of char array of size 10
          */
-        static char[] gameboard = new char[10];
+     static char player, computer;
+     static Scanner scanner = new Scanner(System.in);
+
+     static char[] gameboard = new char[10];
         /**
          *createEmptyBoard Static method to create empty board
          * It takes board array indexes from 1 to 9 using for loop and assign
@@ -14,6 +20,17 @@ public class TicTacToe {
                 gameboard[index] = ' ';
             }
         }
+    public static void chooseLetter() {
+        System.out.println("Please Select Your Choice Letter : 'X' or 'O' ");
+        char choice = scanner.next().charAt(0);
+        if (choice == 'X') {
+            player = 'X';
+            computer = 'O';
+        } else if (choice == 'Y') {
+            player = 'O';
+            computer = 'X';
+        }
+    }
         public static void main(String[] args) {
             System.out.println("Welcome to the TicTacToe Game Program");
             System.out.println(gameboard[1] + " | " + gameboard[2] + " | " + gameboard[3]);
@@ -22,6 +39,7 @@ public class TicTacToe {
             System.out.println("----------");
             System.out.println(gameboard[7] + " | " + gameboard[8] + " | " + gameboard[9]);
             createEmptyBoard();
+            chooseLetter();
           }
 
     }
